1dda48b34d178bf62108319c6bcda110da9470a6
Pavle Zikic <zikicpavle@gmail.com>
2021-02-12 19:14:03 +0100

    Chat and message compoennt init

--- ExtraMessenger/ClientApp/src/app/chat/message-segment/message-segment.component.ts
+++ ExtraMessenger/ClientApp/src/app/chat/message-segment/message-segment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import { MessageService } from 'src/app/services/message.service';
 
 @Component({
@@ -8,13 +10,40 @@ import { MessageService } from 'src/app/services/message.service';
 })
 export class MessageSegmentComponent implements OnInit {
 
-  constructor(private messageService: MessageService) { }
+  messages$: Observable<any[]>; //merge operator
+  messages: any[] = [];
+  messagesSubject = new BehaviorSubject<any[]>([]);
+  threadChange$;
+  socketSubscription: Subscription;
+
+  constructor(public _messageService: MessageService) { } //, private _authService: AuthService
 
   ngOnInit(): void {
+
+    this.messages$ = this.messagesSubject.asObservable();
+
+    this.socketSubscription = this._messageService.messageArrived.asObservable()
+      .pipe(
+        filter(data => data.senderId == this._messageService.messageThread.value.recieverId ) //|| this._authService._decodedToken.nameid == data.senderId
+      ).subscribe(
+        data => {
+          this.messages.unshift(data);
+          this.messagesSubject.next(this.messages)
+        }
+      )
+
+    this.threadChange$ = this._messageService.messageThread
+      .asObservable().subscribe(data => {
+        this._messageService.getMessages().subscribe(data => {
+          this.messagesSubject.next(data)
+          this.messages = data
+        });
+
+      });
   }
 
-  send() {
-    this.messageService.sendMessage(2, 'Whats UPP???')
+  ngOnDestroy(): void {
+    this.threadChange$.unsubscribe();
+    // this.socketSubscription.unsubscribe();
   }
-
 }
--- ExtraMessenger/ClientApp/src/app/chat/sidebar/sidebar.component.ts
+++ ExtraMessenger/ClientApp/src/app/chat/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  contacts$: Observable<any[]>;
+
+  constructor(private _messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.contacts$ = this._messageService.getContacts();
+  }
+
+  openThread(chatInteractionId, recieverId, senderName) {
+    this._messageService.messageThread.next({chatInteractionId: chatInteractionId, recieverId: recieverId, senderName: senderName});
   }
 
 }
