acf1d200a331bc32bd500504a4259e35baa7f144
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-02-13 23:54:09 +0100

    add UserController to get all users

---
+++ ExtraMessenger/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using ExtraMessenger.Data;
+using ExtraMessenger.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using ExtraMessenger.DTOs;
+
+namespace ExtraMessenger.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly MongoService _mongoService;
+
+        public UserController(MongoService mongoService)
+        {
+            _mongoService = mongoService;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var db = _mongoService.GetDb;
+
+            var userCollection = db.GetCollection<User>("Users");
+
+            var users = await userCollection.FindAsync<User>(new BsonDocument());
+
+            var usersList = await users.ToListAsync();
+
+            var usersToReturn = usersList.Select(user => 
+            new UserDTO 
+            { 
+                ObjectId = user.Id.ToString(), 
+                Username = user.Username 
+            });
+
+            return Ok(usersToReturn);
+        }
+    }
+}
---
+++ ExtraMessenger/DTOs/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExtraMessenger.DTOs
+{
+    public class UserDTO
+    {
+        public string ObjectId { get; set; }
+
+        public string Username { get; set; }
+
+    }
+}
